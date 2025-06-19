63f3143ace1467e7c777c292950fb0f15cc7d3d0
Ryeker Herndon <ryekerjh@protonmail.com>
2025-06-19 09:26:54 -0600

    fix(table/key): add keys to mapped values & optional chain ops for possibly undefined, add <tr> to fix render issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -71,25 +71,27 @@ export default function Home() {
       <br />
       <table>
         <thead>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>City</th>
-          <th>Degree</th>
-          <th>Specialties</th>
-          <th>Years of Experience</th>
-          <th>Phone Number</th>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>City</th>
+            <th>Degree</th>
+            <th>Specialties</th>
+            <th>Years of Experience</th>
+            <th>Phone Number</th>
+          </tr>
         </thead>
         <tbody>
           {filteredAdvocates?.map((advocate) => {
             return (
-              <tr>
+              <tr key={advocate.phoneNumber}>
                 <td>{advocate.firstName}</td>
                 <td>{advocate.lastName}</td>
                 <td>{advocate.city}</td>
                 <td>{advocate.degree}</td>
                 <td>
-                  {advocate.specialties.map((s) => (
-                    <div>{s}</div>
+                  {advocate.specialties.map((s, idx) => (
+                    <div key={idx}>{s}</div>
                   ))}
                 </td>
                 <td>{advocate.yearsOfExperience}</td>
